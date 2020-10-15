a00dc71f50b794a21f7b2d0258ce2dc018566d88
Mariano <marianosalasaraoz@gmail.com>
2020-10-15 18:35:58 -0300

    demo3

--- js/index.js
+++ js/index.js
@@ -7,3 +7,23 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+//Traer el boton
+mybutton = document.getElementById("myBtn");
+
+// Mostrar el boton cuando se baje mas de 20px del top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// Ir al top cuando se haga click en el boton
+function topFunction() {
+  document.body.scrollTop = 0; // Para safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE y Opera
+}
